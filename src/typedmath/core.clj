f035a5c6cc2fd5764f52cea85d233711e22dd659
Jonas Ostlund <jonas@Jonass-MacBook-Pro.local>
2016-08-04 17:59:34 +0200

    bra eller

--- src/typedmath/core.clj
+++ src/typedmath/core.clj
@@ -1,13 +1,20 @@
 (ns typedmath.core)
 
+(set! *warn-on-reflection* true)
+
+(defn precompute [x]
+  (try
+    (eval x)
+    (catch Throwable e x)))
+
 ;; Represents any number on the host platform.
 (defn make-number [x]
   (assert (number? x))
-  {:type :number :value x})
+  {:type :number :expr x})
 
 ;; Represents something not known at compile time.
 (defn make-dynamic [x]
-  {:type :dynamic :value x})
+  {:type :dynamic :expr x})
 
 (defn conj-in-map [m key value]
   (if (contains? m key)
@@ -20,10 +27,10 @@
                
 ;; A look-up table of defined functions
 (let [table (atom {})]
-  (defn add-inline-function [name tester? fun]
+  (defn add-typed-inline [name tester? fun]
     (swap! table #(conj-in-map % name [tester? fun])))
 
-  (defn find-inline-function [name args]
+  (defn find-typed-inline [name args]
     (if-let [[tester? fun]
              (first
               (filter 
@@ -32,14 +39,14 @@
                (get (deref table) name)))]
              fun)))
 
-(add-inline-function 'dummy 
+(add-typed-inline 'dummy 
                      (fn [args]
                        (every? number? args))
                      :dummy-numbers)
 
 (assert (= :dummy-numbers
-           (find-inline-function 'dummy [1 2 3])))
-(assert (nil? (find-inline-function 'another-dummy [1 2 3])))
+           (find-typed-inline 'dummy [1 2 3])))
+(assert (nil? (find-typed-inline 'another-dummy [1 2 3])))
 
 (defn valid-type-spec? [x]
   (and (vector? x)
@@ -66,53 +73,124 @@
   ((make-type-tester [[:number 'a] [:scalar 'b]])
    [{:type :number :value 9} {:type :number :value 10}])))
 
-(defmacro defn-typed [name types cb & body]
+(defmacro def-typed-inline [name types cb & body]
   (assert (symbol? name))
   (assert (valid-type-spec? types))
   (assert (symbol? cb))
-  `(add-inline-function 
+  `(add-typed-inline 
     (quote ~name)
     (make-type-tester (quote ~types))
     (fn [[~@(map second types)] ~cb]
       ~@body)))
 
-(defn-typed typed+ [[:number a] [:number b]] cb
+(def-typed-inline typed+ [[:number a] [:number b]] cb
   (cb {:type :number
-       :expr 119})); `(+ ~(:expr a) ~(:expr b))}))
+       :expr (precompute `(+ ~(:expr a) ~(:expr b)))}))
 
-(assert (= {:type :number :value 9}) (make-number 9))
+(assert (= (call-typed-inline 'typed+ [{:type :number :expr 3}
+                                       {:type :number :expr 4}] identity)
+           {:type :number
+            :expr 7}))
 
-(declare compile-form)
+(defn call-typed-inline [name args cb]
+  (if-let [f (find-typed-inline name args)]
+    (f args cb)
+    (RuntimeException.
+     (str "Didn't find function named " name " for arguments " args))))
 
-(defn compile-exprs-sub [args acc cb]
+(assert (= {:type :number :expr 9}) (make-number 9))
+
+(declare compile-expr)
+
+(defn async-map-sub [f args acc cb]
   (if (empty? args)
     (cb acc)
-    (compile-form 
+    (f
      (first args)
      (fn [arg]
-       (compile-exprs-sub (rest args) (conj acc arg) cb)))))
+       (async-map-sub f (rest args) (conj acc arg) cb)))))
+
+(defn async-map [f args cb]
+  (async-map-sub f args [] cb))
 
 ;; Used to compile a sequence of exprs. The compiled
 ;; exprs are passed as a vector to the callback cb.
 (defn compile-exprs [exprs cb]
-  (compile-exprs-sub exprs [] cb))
+  (async-map compile-expr exprs cb))
 
 (assert (= [{:type :number :value 9} {:type :number :value 11}])
         (compile-exprs [9 11] identity))
 
-(defn compile-form [x cb]
+(defn make-vector [v0 cb]
+  (compile-exprs 
+   v0 (fn [v] (cb {:type :vector :fields v}))))
+
+(defn typed-inline-call? [x]
+  (and (seq? x)
+       (not (nil? (find-typed-inline (first x)
+                                     (rest x))))))
+
+(defn make-typed-inline-call [[name & args] cb]
+  (call-typed-inline name args cb))
+
+(defn compile-list-form [x cb]
+  ;; Currently, only typed calls.
+  (let [[name & args] x]
+    (compile-exprs 
+     args
+     (fn [cargs]
+       (call-typed-inline name cargs cb)))))
+  
+(defn compile-expr [x cb]
   (cond
     (number? x) (cb (make-number x))
     (symbol? x) (cb (make-dynamic x))
-    :default "Failed"))
+    (vector? x) (make-vector x cb)
+    (list? x) (compile-list-form x cb)
+    :default (RuntimeException. (str "Failed to compile: " x))))
 
-(assert [:this-is-a-number {:type :number :value 9}]
-        (compile-form 9 (fn [x] [:this-is-a-number x])))
+(assert (= [:this-is-a-number {:type :number :expr 9}]
+           (compile-expr 9 (fn [x] [:this-is-a-number x]))))
+(assert (= (compile-expr [1 2 3] identity)
+           {:type :vector, :fields [{:type :number, :expr 1} 
+                                    {:type :number, :expr 2} 
+                                    {:type :number, :expr 3}]}))
+(assert (= (compile-expr [[1 2] 3] identity)
+           {:type :vector, :fields [{:type :vector, :fields 
+                                     [{:type :number, :expr 1} 
+                                      {:type :number, :expr 2}]} 
+                                    {:type :number, :expr 3}]}))
+(assert (= (compile-expr '(typed+ 1 2) identity)
+           {:type :number, :expr 3}))
 
 
 
-;(defn compile-forms [frms]
+(def-typed-inline typed+ [[:vector a] [:number b]] cb
+  (async-map
+   (fn [field cb] 
+     (call-typed-inline 'typed+ [field b] cb))
+   (:fields a)
+   (fn [added]
+     (cb {:type :vector
+          :fields added}))))
+
+(assert (= (compile-expr '(typed+ [1 2 3] 4) identity)
+           '{:type :vector, 
+            :fields [{:type :number, 
+                      :expr 5} 
+                     {:type :number, :expr 6} 
+                     {:type :number, :expr 7}]}))
+
+(defmulti make-expression :type :default nil)
+(defmethod make-expression :number [x] (:expr x))
+(defmethod make-expression :vector [x] (mapv make-expression (:fields x)))
+
+(assert (= [5 6 7]
+           (eval (make-expression 
+                  (compile-expr '(typed+ [1 2 3] 4) identity)))))
+
+;(defn compile-exprs [frms]
 ;  (
 
 ;(defmacro statically [& frms]
-;  (compile-forms frms))
+;  (compile-exprs frms))
---
+++ test/typedmath/core_test.clj
@@ -0,0 +1,7 @@
+(ns typedmath.core-test
+  (:require [clojure.test :refer :all]
+            [typedmath.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
