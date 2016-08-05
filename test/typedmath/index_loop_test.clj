d723ab50cbd187342dd6e77d389293f3c95cbfc8
Jonas Ostlund <jonas@Jonass-MacBook-Pro.local>
2016-08-05 13:40:55 +0200

    good stuff

--- test/typedmath/index_loop_test.clj
+++ test/typedmath/index_loop_test.clj
@@ -1,2 +1,10 @@
 (ns typedmath.index-loop-test
-  (:require [typedmath.index-loop :refer :all]))
+  (:require [typedmath.index-loop :refer :all])
+  (:require [clojure.test :refer :all]))
+
+(deftest index-loop-test
+  (testing "Looping and indexing"
+    (is (= '(:typedmath.index-loop/add 
+             (:typedmath.index-loop/mul 
+              3 9 (:typedmath.index-loop/add 4 5)) 4)
+           (standardize '(+ (* 3 9 (+ 4 5)) 4))))))
