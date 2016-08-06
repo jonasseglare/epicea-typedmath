00f300d572430ece73071137071fc97d2ce4fc3c
Jonas Ostlund <jonas@Jonass-MacBook-Pro.local>
2016-08-06 13:18:11 +0200

    this looks good

--- test/typedmath/index_loop_test.clj
+++ test/typedmath/index_loop_test.clj
@@ -1,4 +1,4 @@
-(ns typedmath.index-loop-test
+,(ns typedmath.index-loop-test
   (:require [typedmath.index-loop :refer :all])
   (:require [clojure.test :refer :all]))
 
@@ -9,4 +9,13 @@
               3 9 (:typedmath.index-loop/add 4 5)) 4)
            (standardize '(+ (* 3 9 (+ 4 5)) 4))))
     (is (= (flatten-sub-expr '(:typedmath.index-loop/add a b))
-           '(:typedmath.index-loop/add a b)))))
+           '(:typedmath.index-loop/add a b)))
+    (is (= '(:typedmath.index-loop/add 1 2 3)
+           (flatten-expr (standardize '(+ 1 (+ 2 3))))))
+    (is (= (mul-2 '(:typedmath.index-loop/add  3 4) 
+                  '(:typedmath.index-loop/add  5 6))
+           '(:typedmath.index-loop/add 
+             (:typedmath.index-loop/mul 3 5) 
+             (:typedmath.index-loop/mul 3 6) 
+             (:typedmath.index-loop/mul 4 5) 
+             (:typedmath.index-loop/mul 4 6))))))
