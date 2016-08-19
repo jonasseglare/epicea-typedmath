f87ee5cd687ab4c4772a9b007ee448e6f0e8ccfe
Jonas Ostlund <jonas@Jonass-MacBook-Pro.local>
2016-08-19 07:50:57 +0200

    works

--- test/typedmath/core_test.clj
+++ test/typedmath/core_test.clj
@@ -20,8 +20,6 @@
     (let [^"[D" B (make-array Double/TYPE 3)]
       (is (instance? java.lang.Double (aget B 0))))))
 
-(comment
-
 (deftest typedmath-test
   (testing "Inlining"
     (add-typed-inline 'dummy 
@@ -31,134 +29,140 @@
     (is (= {:katt [119]} (conj-in-map {} :katt 119)))
     (is (= {:katt [119 120]} 
            (conj-in-map {:katt [119]} :katt 120)))
-    (is (= :dummy-numbers
-           (find-typed-inline 'dummy [1 2 3])))
-    (is (nil? (find-typed-inline 'another-dummy [1 2 3])))
-    (is (valid-type-spec? [[:double 'a] [:scalar 'b]]))
-    (is (not (valid-type-spec? [[1] [:double 'b]])))
-    (is ((make-type-tester [[:double 'a] [:double 'b]])
-             [{:type :double :value 9} {:type :double :value 10}]))
-    (is
-     (not 
-      ((make-type-tester [[:double 'a] [:scalar 'b]])
-       [{:type :double :value 9} {:type :double :value 10}])))
-    (is (= (call-typed-inline 'typed+ [{:type :double :expr 3}
-                                       {:type :double :expr 4}] identity)
-           {:type :double
-            :expr 7}))
+    ;; (is (= :dummy-numbers
+    ;;        (find-typed-inline 'dummy [1 2 3])))
+    ;; (is (nil? (find-typed-inline 'another-dummy [1 2 3])))
+    ;; (is (valid-type-spec? [[:double 'a] [:scalar 'b]]))
+    ;; (is (not (valid-type-spec? [[1] [:double 'b]])))
+    ;; (is ((make-type-tester [[:double 'a] [:double 'b]])
+    ;;          [{:type :double :value 9} {:type :double :value 10}]))
+    ;; (is
+    ;;  (not 
+    ;;   ((make-type-tester [[:double 'a] [:scalar 'b]])
+    ;;    [{:type :double :value 9} {:type :double :value 10}])))
+    ;; (is (= (call-typed-inline 'typed+ [{:type :double :expr 3}
+    ;;                                    {:type :double :expr 4}] identity)
+    ;;        {:type :double
+    ;;         :expr 7}))
 
-    (is (= {:type :double :expr 9}) (make-number-type 9))
-    (is (= [{:type :double :value 9} {:type :double :value 11}])
-        (compile-exprs {} [9 11] (fn [_ x] x)))
+    ;; (is (= {:type :double :expr 9}) (make-number-type 9))
+    ;; (is (= [{:type :double :value 9} {:type :double :value 11}])
+    ;;     (compile-exprs {} [9 11] (fn [_ x] x)))
 
-    (is (= [:this-is-a-number {:type :double :expr 9}]
-               (compile-expr1 {} 9 (fn [x] [:this-is-a-number x]))))
-    (is (= (compile-expr1 {} [1 2 3] identity)
-               {:type :vector, :fields [{:type :double, :expr 1} 
-                                        {:type :double, :expr 2} 
-                                        {:type :double, :expr 3}]}))
-    (is (= (compile-expr1 {} [[1 2] 3] identity)
-               {:type :vector, :fields [{:type :vector, :fields 
-                                         [{:type :double, :expr 1} 
-                                          {:type :double, :expr 2}]} 
-                                        {:type :double, :expr 3}]}))
-    (is (= (compile-expr1 {} '(typed+ 1 2) identity)
-               {:type :double, :expr 3}))
+    ;; (is (= [:this-is-a-number {:type :double :expr 9}]
+    ;;            (compile-expr1 {} 9 (fn [x] [:this-is-a-number x]))))
+    ;; (is (= (compile-expr1 {} [1 2 3] identity)
+    ;;            {:type :vector, :fields [{:type :double, :expr 1} 
+    ;;                                     {:type :double, :expr 2} 
+    ;;                                     {:type :double, :expr 3}]}))
+    ;; (is (= (compile-expr1 {} [[1 2] 3] identity)
+    ;;            {:type :vector, :fields [{:type :vector, :fields 
+    ;;                                      [{:type :double, :expr 1} 
+    ;;                                       {:type :double, :expr 2}]} 
+    ;;                                     {:type :double, :expr 3}]}))
+    ;; (is (= (compile-expr1 {} '(typed+ 1 2) identity)
+    ;;            {:type :double, :expr 3}))
 
-    (is (= (compile-expr1 {} '(typed+ [1 2 3] 4) identity)
-               '{:type :vector, 
-                 :fields [{:type :double, 
-                           :expr 5} 
-                          {:type :double, :expr 6} 
-                          {:type :double, :expr 7}]}))
-    (is (= [5 6 7]
-           (eval (make-clojure-data 
-                  (compile-expr1 {} '(typed+ [1 2 3] 4) identity)))))
+    ;; (is (= (compile-expr1 {} '(typed+ [1 2 3] 4) identity)
+    ;;            '{:type :vector, 
+    ;;              :fields [{:type :double, 
+    ;;                        :expr 5} 
+    ;;                       {:type :double, :expr 6} 
+    ;;                       {:type :double, :expr 7}]}))
+    ;; (is (= [5 6 7]
+    ;;        (eval (make-clojure-data 
+    ;;               (compile-expr1 {} '(typed+ [1 2 3] 4) identity)))))
 
-    (is (= (replace-recursively {:a 3 :b 4} [:a :b])
-           [3 4]))
+    ;; (is (= (replace-recursively {:a 3 :b 4} [:a :b])
+    ;;        [3 4]))
     
-    (is (= (compile-expr1 {} '(typed* 9 3) identity)
-           '{:type :double :expr 27}))
+    ;; (is (= (compile-expr1 {} '(typed* 9 3) identity)
+    ;;        '{:type :double :expr 27}))
 
-    (is (= [-1 -2 -3] 
-           (make-clojure-data
-            (compile-expr1 {} '(typed- [5 4 3] 6) identity))))
-    (is (= [2 4 8]
-           (make-clojure-data
-            (compile-expr1 {} '(typed* 2 [1 2 4]) identity))))
+    ;; (is (= [-1 -2 -3] 
+    ;;        (make-clojure-data
+    ;;         (compile-expr1 {} '(typed- [5 4 3] 6) identity))))
+    ;; (is (= [2 4 8]
+    ;;        (make-clojure-data
+    ;;         (compile-expr1 {} '(typed* 2 [1 2 4]) identity))))
 
-    (is (= (drop-data (compile-expr1 {} '[1 2 3] identity))
-           {:type :vector, :fields [{:type :double} {:type :double} {:type :double}]}))
+    ;; (is (= (drop-data (compile-expr1 {} '[1 2 3] identity))
+    ;;        {:type :vector, :fields [{:type :double} {:type :double} {:type :double}]}))
 
-    (is (= 3 (flat-size (compile-expr1 {} '[1 2 3] identity))))
-    (is (= [1 2 3 4 5] (flat-vector (compile-expr1 {} '[1 2 [[3] 4 5]] identity))))
+    ;; (is (= 3 (flat-size (compile-expr1 {} '[1 2 3] identity))))
+    ;; (is (= [1 2 3 4 5] (flat-vector (compile-expr1 {} '[1 2 [[3] 4 5]] identity))))
 
-    (is (= (populate {:type :double} [9])
-           {:type :double :expr 9}))
-    (let [my-type (drop-data (compile-expr1 {} '[1 [2 3]] identity))]
-      (is (= my-type {:type :vector, :fields 
-                      [{:type :double} {:type :vector, :fields 
-                                        [{:type :double} {:type :double}]}]}))
-      (is (= (compile-expr1 {} '[9 [20 119]] identity)
-             (populate my-type [9 20 119]))))
+    ;; (is (= (populate {:type :double} [9])
+    ;;        {:type :double :expr 9}))
+    ;; (let [my-type (drop-data (compile-expr1 {} '[1 [2 3]] identity))]
+    ;;   (is (= my-type {:type :vector, :fields 
+    ;;                   [{:type :double} {:type :vector, :fields 
+    ;;                                     [{:type :double} {:type :double}]}]}))
+    ;;   (is (= (compile-expr1 {} '[9 [20 119]] identity)
+    ;;          (populate my-type [9 20 119]))))
 
-    (is (= (compile-expr1 {} '[9 [4 5 6] 7 8 9] identity)
-           (populate (drop-data (compile-expr1 {} '[0 [0 0 0] 0 0 0] identity))
-                     [9 4 5 6 7 8 9])))
-    (is (= 3 (get-primitive {:type :double :expr 3})))
-    (is (= {:a 3} (compile-expr1 {} ''{:a 3} identity)))
-    (is (= {:a 3} (compile-expr1 {} '(quote {:a 3}) identity)))
-    (is (= (statically (output-value [3 4 5]))
-           [3 4 5]))
-    (is (= (statically [3 4 5])
-           {:type :vector
-            :fields [{:type :double, :expr 3} 
-                     {:type :double, :expr 4} 
-                     {:type :double, :expr 5}]}))
-    (is (= [4 5 6] (statically (output-value (typed+ 1 [3 4 5])))))
-    (is (= (bind-context {} 'rulle {:type :double :expr 3})
-           {:bindings {'rulle {:type :double :expr 3}}}))
-    (is (= (let [a 9] (statically (input-value {:type :double} a) a))
-           {:type :double, :expr 9}))
-    (is (= (let [a 9] (statically (input-value {:type :double} a)))
-           {:type :double :expr 9}))
-    (is (= {:type :double :value -9.0}) (statically (typed- 9.0)))
-    (is (= (let [a 2
-                 b 3]
-             (statically 
-              (input-value {:type :double} a)
-              (input-value {:type :double} b)
-              (typed+ a b)))
-           {:type :double :expr 5}))
-    (is (= (statically (typed+ (input-value {:type :double} 3) 
-                               (input-value {:type :double} 4)))
-           {:type :double, :expr 7}))
-    (is (= (let [A [1 2 3 4]]
-             (statically 
-              (output-value
-               (typed+ 
-                9 (input-value {:type :vector 
-                            :fields [{:type :double} 
-                                     {:type :double} 
-                                     {:type :double} 
-                                     {:type :double}]} A)))))
-           [10 11 12 13]))
-    (is (= (sized-vector-type {:type :double} 3)
-           '{:type :vector, :fields ({:type :double} {:type :double} {:type :double})}))
+    ;; (is (= (compile-expr1 {} '[9 [4 5 6] 7 8 9] identity)
+    ;;        (populate (drop-data (compile-expr1 {} '[0 [0 0 0] 0 0 0] identity))
+    ;;                  [9 4 5 6 7 8 9])))
+    ;; (is (= 3 (get-primitive {:type :double :expr 3})))
+    ;; (is (= {:a 3} (compile-expr1 {} ''{:a 3} identity)))
+    ;; (is (= {:a 3} (compile-expr1 {} '(quote {:a 3}) identity)))
+    ;; (is (= (statically (output-value [3 4 5]))
+    ;;        [3 4 5]))
+    ;; (is (= (statically [3 4 5])
+    ;;        {:type :vector
+    ;;         :fields [{:type :double, :expr 3} 
+    ;;                  {:type :double, :expr 4} 
+    ;;                  {:type :double, :expr 5}]}))
+    ;; (is (= [4 5 6] (statically (output-value (typed+ 1 [3 4 5])))))
+    ;; (is (= (bind-context {} 'rulle {:type :double :expr 3})
+    ;;        {:bindings {'rulle {:type :double :expr 3}}}))
+    ;; (is (= (let [a 9] (statically (input-value {:type :double} a) a))
+    ;;        {:type :double, :expr 9}))
+    ;; (is (= (let [a 9] (statically (input-value {:type :double} a)))
+    ;;        {:type :double :expr 9}))
+    ;; (is (= {:type :double :value -9.0}) (statically (typed- 9.0)))
+    ;; (is (= (let [a 2
+    ;;              b 3]
+    ;;          (statically 
+    ;;           (input-value {:type :double} a)
+    ;;           (input-value {:type :double} b)
+    ;;           (typed+ a b)))
+    ;;        {:type :double :expr 5}))
+    ;; (is (= (statically (typed+ (input-value {:type :double} 3) 
+    ;;                            (input-value {:type :double} 4)))
+    ;;        {:type :double, :expr 7}))
+    ;; (is (= (let [A [1 2 3 4]]
+    ;;          (statically 
+    ;;           (output-value
+    ;;            (typed+ 
+    ;;             9 (input-value {:type :vector 
+    ;;                         :fields [{:type :double} 
+    ;;                                  {:type :double} 
+    ;;                                  {:type :double} 
+    ;;                                  {:type :double}]} A)))))
+    ;;        [10 11 12 13]))
+    ;; (is (= (sized-vector-type {:type :double} 3)
+    ;;        '{:type :vector, :fields ({:type :double} {:type :double} {:type :double})}))
 
-    (is (= (let [A [3 4 5]]
-             (statically 
-              (output-value
-               (typed+
-                9 (input-value (sized-vector-type {:type :double} 3) A)))))
-           [12 13 14]))
+    ;; (is (= (let [A [3 4 5]]
+    ;;          (statically 
+    ;;           (output-value
+    ;;            (typed+
+    ;;             9 (input-value (sized-vector-type {:type :double} 3) A)))))
+    ;;        [12 13 14]))
 
-    (is (= (Math/sin (* 0.25 Math/PI))
-           (statically (output-value (Math/sin (* 0.25 Math/PI))))))
-    (is (= {:type :double :expr 6} (statically (typed+ 1 2 3))))
+    ;; (is (= (Math/sin (* 0.25 Math/PI))
+    ;;        (statically (output-value (Math/sin (* 0.25 Math/PI))))))
+    ;; (is (= {:type :double :expr 6} (statically (typed+ 1 2 3))))
 
-))
+    ))
+
+
+
+
+(comment
+
 
 (defn assign-test []
   (let [A (allocate-ndarray [2 3] {:type :double})
